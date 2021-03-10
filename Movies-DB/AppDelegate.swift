a50f88cb9f057c07ef051f7f464a1e9b992074e4
Abdelrahman Sobhy <abdelrahman.raye7@gmail.com>
2021-03-10 17:04:21 +0200

    init

---
+++ Movies-DB/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Movies-DB
+//
+//  Created by Abdelrahman Sobhy on 3/10/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
